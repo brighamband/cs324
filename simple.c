056607b92b6153f315f1d35050802637cb4307e4
Brigham Andersen <brighamband@gmail.com>
2021-08-31 17:24:56 -0600

    prints process ids

--- simple.c
+++ simple.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <unistd.h>
+
+int printProcessId() {
+  int pid = fork();
+
+    if (pid == 0) {
+    printf("Parent Process id : %d\n", getpid());
+    printf("Child Process with parent id : %d\n", getppid());
+  }
+}
 
 int main() {
   puts("hello");
 
+  printProcessId();
+
   return 0;
 }
+
