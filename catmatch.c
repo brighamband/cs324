98e0c75bdbbd438c133b52b9d926bb1700455d1c
Brigham Andersen <brighamband@gmail.com>
2021-08-31 20:03:07 -0600

    Almost done, just duplicating (twice)

--- catmatch.c
+++ catmatch.c
@@ -1,7 +1,11 @@
 /* Use this command to compile catmatch:  gcc catmatch.c -o catmatch (add '&& ./catmatch {filename}' to execute in one step) */
+/* Must reset CATMATCH_PATTERN env var every session using export CATMATCH_PATTERN='linux' */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 
 int printProcessId() {
   int pid = fork();
@@ -12,26 +16,38 @@ int printProcessId() {
 }
 
 int main(int argc, char* argv []) {
-  printProcessId();
-
   if (argc == 1) {
     puts("Make sure to specify a filename.");
     return 1;
   }
+  printProcessId();
 
-  FILE* file;
+  FILE* fp;
   char str[100];
 
-  file = fopen(argv[1], "r");
-  if (file == NULL) {
+  fp = fopen(argv[1], "r");
+  if (fp == NULL) {
     perror("Error opening file");
     return 1;
-  } 
-
-  if (fgets(str, 100, file) != NULL) {
-    puts(str);
   }
-  fclose(file);
+
+  char* catmatchPattern = getenv("CATMATCH_PATTERN");
+
+  while(fgets(str, sizeof str, fp)) {
+    int marker = 0;
+    if (catmatchPattern != NULL) {
+      char* foundPtr = strstr(str, catmatchPattern);
+      if (foundPtr != NULL) marker = 1;
+    }
+    printf("%d %s", marker, str);
+  }
+  printf("\n");
+  fclose(fp);
+
+  // if (fgets(str, 100, file) != NULL) {
+  //   puts(str);
+  // }
+  // fclose(file);
 
   return 0;
 }
