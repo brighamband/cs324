ec97b16846ddb4bdc2fe7fd227194912cf78a06f
Brigham Andersen <brighamband@gmail.com>
2021-09-14 06:40:29 -0600

    15 done

--- hw3/forkexec/fork.c
+++ hw3/forkexec/fork.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -9,12 +9,18 @@ int main(int argc, char *argv[]) {
 
 	printf("Starting program; process has pid %d\n", getpid());
 
+	FILE * fp = fopen("fork-output.txt", "w");
+	fprintf(fp, "BEFORE FORK\n");
+	fflush(fp);
+
 	if ((pid = fork()) < 0) {
 		fprintf(stderr, "Could not fork()");
 		exit(1);
 	}
 
 	/* BEGIN SECTION A */
+	fprintf(fp, "SECTION A\n");
+	fflush(fp);
 
 	printf("Section A;  pid %d\n", getpid());
 	sleep(30);
@@ -22,10 +28,12 @@ int main(int argc, char *argv[]) {
 	/* END SECTION A */
 	if (pid == 0) {
 		/* BEGIN SECTION B */
+		fprintf(fp, "SECTION B\n");
+		fflush(fp);
 
 		printf("Section B\n");
-		sleep(30);
-		sleep(30);
+		// sleep(30);
+		// sleep(30);
 		printf("Section B done sleeping\n");
 
 		exit(0);
@@ -33,10 +41,12 @@ int main(int argc, char *argv[]) {
 		/* END SECTION B */
 	} else {
 		/* BEGIN SECTION C */
+		fprintf(fp, "SECTION C\n");
+		fflush(fp);
 
 		printf("Section C\n");
-		sleep(30);
-		// wait(NULL);
+		// sleep(30);
+		wait(NULL);
 		// sleep(30);
 		printf("Section C done sleeping\n");
 
@@ -45,6 +55,8 @@ int main(int argc, char *argv[]) {
 		/* END SECTION C */
 	}
 	/* BEGIN SECTION D */
+	fprintf(fp, "SECTION D\n");
+	fflush(fp);
 
 	printf("Section D\n");
 	sleep(30);
