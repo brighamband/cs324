41a5a5cf9ff2caa272952d2835215a3cfbc2e550
Brigham Andersen <brighamband@gmail.com>
2021-09-14 07:02:01 -0600

    16 done

--- hw3/forkexec/fork.c
+++ hw3/forkexec/fork.c
@@ -6,12 +6,15 @@
 
 int main(int argc, char *argv[]) {
 	int pid;
+	int pipefd[2];
 
 	printf("Starting program; process has pid %d\n", getpid());
 
-	FILE * fp = fopen("fork-output.txt", "w");
-	fprintf(fp, "BEFORE FORK\n");
-	fflush(fp);
+	FILE * forkOut = fopen("fork-output.txt", "w");
+	fprintf(forkOut, "BEFORE FORK\n");
+	fflush(forkOut);
+
+	pipe(pipefd);
 
 	if ((pid = fork()) < 0) {
 		fprintf(stderr, "Could not fork()");
@@ -19,8 +22,8 @@ int main(int argc, char *argv[]) {
 	}
 
 	/* BEGIN SECTION A */
-	fprintf(fp, "SECTION A\n");
-	fflush(fp);
+	fprintf(forkOut, "SECTION A\n");
+	fflush(forkOut);
 
 	printf("Section A;  pid %d\n", getpid());
 	sleep(30);
@@ -28,8 +31,12 @@ int main(int argc, char *argv[]) {
 	/* END SECTION A */
 	if (pid == 0) {
 		/* BEGIN SECTION B */
-		fprintf(fp, "SECTION B\n");
-		fflush(fp);
+		fprintf(forkOut, "SECTION B\n");
+		fflush(forkOut);
+
+		close(pipefd[0]);	// Close read end
+		FILE * pipeOut = fdopen(pipefd[1], "w");	// Make file stream for write end
+		fputs("hello from Section B\n", pipeOut);	// Write msg to write end
 
 		printf("Section B\n");
 		// sleep(30);
@@ -41,8 +48,14 @@ int main(int argc, char *argv[]) {
 		/* END SECTION B */
 	} else {
 		/* BEGIN SECTION C */
-		fprintf(fp, "SECTION C\n");
-		fflush(fp);
+		fprintf(forkOut, "SECTION C\n");
+		fflush(forkOut);
+
+		close(pipefd[1]);	// Close write end
+		FILE * pipeIn = fdopen(pipefd[0], "r");	// Make file stream for read end
+		char buf[100];	// Read in a line, print to stdout
+    fgets(buf, 100, pipeIn);
+    printf("%s", buf);
 
 		printf("Section C\n");
 		// sleep(30);
@@ -55,8 +68,8 @@ int main(int argc, char *argv[]) {
 		/* END SECTION C */
 	}
 	/* BEGIN SECTION D */
-	fprintf(fp, "SECTION D\n");
-	fflush(fp);
+	fprintf(forkOut, "SECTION D\n");
+	fflush(forkOut);
 
 	printf("Section D\n");
 	sleep(30);
