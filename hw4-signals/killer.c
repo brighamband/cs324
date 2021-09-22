8bbd4cb6e0ed74ff32efba54640edf6a3b3b0fa0
Brigham Andersen <brighamband@gmail.com>
2021-09-22 13:25:28 -0600

    added comments to 6

--- hw4-signals/killer.c
+++ hw4-signals/killer.c
@@ -34,13 +34,13 @@ int main(int argc, char *argv[]) {
 		kill(pid, SIGSTKFLT);
 		break;
 	case '6':
-		kill(pid, SIGHUP);
+		kill(pid, SIGHUP);	// Outputs 1 and 2
 		sleep(6);
-		kill(pid, SIGSYS);
-		kill(pid, SIGUSR1);
+		kill(pid, SIGSYS);	// Silences the 7 child output
+		kill(pid, SIGUSR1);	// Makes foo > 0
 		sleep(2);
-		kill(pid, SIGPWR);
-		kill(pid, SIGTERM);
+		kill(pid, SIGPWR);	// Sets foo = 6 (since it's > 0)
+		kill(pid, SIGTERM);	// Prints foo (6)
 		break;
 	case '7':
 		kill(pid, SIGQUIT);
