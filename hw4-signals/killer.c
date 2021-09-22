109efde9c303488442a5c043b3c15ff4bb241dea
Brigham Andersen <brighamband@gmail.com>
2021-09-22 17:58:18 -0600

    fixed sleeps

--- hw4-signals/killer.c
+++ hw4-signals/killer.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
 		break;
 	case '4':
 		kill(pid, SIGHUP);	// Starts outputting 1 and 2
-		sleep(1);		// Gives time for 1 to output from previous signal
+		sleep(1);		// Gives time for target to receive signal
 		kill(pid, SIGINT);	// Starts outputting 1 and 2
 		sleep(5);		// Before exiting, the 2 from the first signal prints as well as the 2 from the last signal
 		break;
@@ -30,21 +30,25 @@ int main(int argc, char *argv[]) {
 		kill(pid, SIGHUP);	// Outputs 1 and 2
 		sleep(5);		// Gives time for them to completely finish
 		kill(pid, SIGUSR1);	// Prints a 7 (forks, then prints the child exit code)
-		sleep(1);		// Lets waiting for child/forking finish first before continuing
+		sleep(1);		// Gives time for target to receive signal
 		kill(pid, SIGSTKFLT);	// Prints a 10 (tries to wait, but WNOHANG returns immediately, so returns 10 error code for no child processes)
+		sleep(1);		// Gives time for target to receive signal
 		break;
 	case '6':
 		kill(pid, SIGHUP);	// Outputs 1 and 2
 		sleep(5);		// Gives time for them to completely finish
 		kill(pid, SIGSYS);	// Blocks the 7 child output
+		sleep(1);		// Gives time for target to receive signal
 		kill(pid, SIGUSR1);	// Makes foo > 0
-		sleep(1);		// Gives time for foo to update before proceeding
+		sleep(1);		// Gives time for target to receive signal
 		kill(pid, SIGPWR);	// Sets foo = 6 (since it's > 0)
+		sleep(1);		// Gives time for target to receive signal
 		kill(pid, SIGTERM);	// Prints foo (6)
+		sleep(1);		// Gives time for target to receive signal
 		break;
 	case '7':
 		kill(pid, SIGSYS);	// Blocks the SIGINT signal that would've outputted 1 and 2
-		sleep(1);		// Give time for block to be set before running next kill signal
+		sleep(1);		// Gives time for target to receive signal
 		kill(pid, SIGQUIT);	// Outputs 8 and 9 (call to SIGINT doesn't go through immediately BC of block)
 		sleep(5);		// Gives time to output 8 and 9 since they sleep
 		kill(pid, SIGSYS);		// Unblock the SIGINT signal (now 1 and 2 will output)
