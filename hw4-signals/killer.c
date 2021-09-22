df894de9f91b4e5aa964d649bbab8e90534e3711
Brigham Andersen <brighamband@gmail.com>
2021-09-22 13:37:02 -0600

    optimized sleeps for 1-5

--- hw4-signals/killer.c
+++ hw4-signals/killer.c
@@ -12,26 +12,26 @@ int main(int argc, char *argv[]) {
 		break;	// No output
 	case '2':
 		kill(pid, SIGHUP);	// Outputs 1 and 2
-		sleep(6);		// Gives time for both 1 and 2 to output before exiting
+		sleep(5);		// Gives time for both 1 and 2 to output before exiting
 		break;
 	case '3':
 		kill(pid, SIGHUP);	// Outputs 1 and 2
-		sleep(6);		// Lets them finish outputting
+		sleep(5);		// Lets them finish outputting
 		kill(pid, SIGHUP);	// Outputs 1 and 2 again (can reuse the same signal since the first one has finished processing)
-		sleep(6);		// Lets them finish outputting
+		sleep(5);		// Lets them finish outputting
 		break;
 	case '4':
 		kill(pid, SIGHUP);	// Starts outputting 1 and 2
-		sleep(2);		// Gives time for 1 to output from previous signal
+		sleep(1);		// Gives time for 1 to output from previous signal
 		kill(pid, SIGINT);	// Starts outputting 1 and 2
-		sleep(6);		// Before exiting, the 2 from the first signal prints as well as the 2 from the last signal
+		sleep(5);		// Before exiting, the 2 from the first signal prints as well as the 2 from the last signal
 		break;
 	case '5':
-		kill(pid, SIGHUP);
-		sleep(6);
-		kill(pid, SIGUSR1);
-		sleep(5);
-		kill(pid, SIGSTKFLT);
+		kill(pid, SIGHUP);	// Outputs 1 and 2
+		sleep(5);		// Gives time for them to completely finish
+		kill(pid, SIGUSR1);	// Prints a 7 (forks, then prints the child exit code)
+		sleep(1);		// Lets waiting for child/forking finish first before continuing
+		kill(pid, SIGSTKFLT);	// Prints a 10 (tries to wait, but WNOHANG returns immediately, so returns 10 error code for no child processes)
 		break;
 	case '6':
 		kill(pid, SIGHUP);	// Outputs 1 and 2
