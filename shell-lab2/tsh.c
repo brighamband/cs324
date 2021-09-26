afd77eb9da79e7d6699cdccfc9a2e8f85d53281c
Brigham Andersen <brighamband@gmail.com>
2021-09-25 20:16:32 -0600

    Moved lab 1 files to lab 2

--- shell-lab2/tsh.c
+++ shell-lab2/tsh.c
@@ -18,6 +18,11 @@
 #define MAXARGS     128   /* max args on a command line */
 #define MAXJOBS      16   /* max jobs at any point in time */
 #define MAXJID    1<<16   /* max job ID */
+#define READ_END      0   /* read end of pipe */
+#define WRITE_END     1   /* write end of pipe */
+#define CHILD_PROCESS 0   /* child process of fork */
+#define TRUE          1   /* boolean substitute */
+#define FALSE         0   /* boolean substitute */
 
 /* Job states */
 #define UNDEF 0 /* undefined */
@@ -86,6 +91,19 @@ void unix_error(char *msg);
 void app_error(char *msg);
 typedef void handler_t(int);
 handler_t *Signal(int signum, handler_t *handler);
+
+/* EXTRA WRAPPER FUNCTIONS FOR SYSTEM CALLS - See bottom of file for function bodies */
+pid_t Fork(void);
+int Dup2(int fd1, int fd2);
+int Pipe(int* pipedes);
+void Execve(const char *filename, char *const argv[], char *const envp[]);
+pid_t Wait(int *status);
+pid_t Waitpid(pid_t pid, int *iptr, int options);
+FILE* Fopen(const char *filename, const char *mode);
+void Fclose(FILE *fp);
+void Setpgid(pid_t pid, pid_t pgid);
+void Close(int fd);
+
 
 /*
  * main - The shell's main routine 
@@ -167,6 +185,98 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+    char *argv[MAXARGS];
+    pid_t pid;
+    int cmds[MAXARGS];
+    int stdin_redir[MAXARGS];
+    int stdout_redir[MAXARGS];
+    pid_t childPids[MAXARGS];
+    int pipes[MAXARGS][2];  // Making max number of pipes, can also just juggle 2 pipes like pipes[2].  Make sure to initialize numCmds - 1 pipes
+    pid_t groupId;
+
+    parseline(cmdline, argv); 
+
+    // Ignore empty lines
+    if (argv[0] == NULL)
+        return;
+
+    // If it's a built-in cmd
+    if (builtin_cmd(argv) == 1) 
+        return;
+
+    // If a custom (not built-in) cmd, run this
+    int numCmds = parseargs(argv, cmds, stdin_redir, stdout_redir); 
+
+    // Make pipes (num of pipes is 1 less than numCmds - they go between cmds)
+    for (int i = 0; i < numCmds - 1; i++) {
+        Pipe(pipes[i]);
+    }
+
+    for (int i = 0; i < numCmds; i++) {
+        pid = Fork();
+
+        // Child (for each cmd)
+        if (pid == CHILD_PROCESS) {
+            int redirectedInput = FALSE;
+            int redirectedOutput = FALSE;
+            
+            // If there's an input file, redirect stdin to input file
+            if (stdin_redir[i] > 0) {
+                redirectedInput = TRUE;
+                FILE *inFile = Fopen(argv[stdin_redir[i]], "r");
+                Dup2(fileno(inFile), STDIN_FILENO);
+                Fclose(inFile);
+            }
+
+            // If there's an output file, redirect stdout to output file
+            if (stdout_redir[i] > 0) {
+                redirectedOutput = TRUE;
+                FILE *outFile = Fopen(argv[stdout_redir[i]], "w");
+                Dup2(fileno(outFile), STDOUT_FILENO);
+                Fclose(outFile);
+            }
+
+            // Redirect stdout to write end (run this for all pipes but the last and if the output wasn't redirected to a file)
+            if (i < numCmds - 1 && !redirectedOutput) {
+                Dup2(pipes[i][WRITE_END], STDOUT_FILENO);
+            }
+
+            // Redirect stdin to read end (run this for all pipes but the first and if the input wasn't redirected to a file)
+            if (i > 0 && !redirectedInput) {
+                Dup2(pipes[i - 1][READ_END], STDIN_FILENO);
+            }
+        
+            // Close all pipes (even the ones you never used)
+            for (int j = 0; j < numCmds - 1; j++) {
+                Close(pipes[j][READ_END]);
+                Close(pipes[j][WRITE_END]);
+            }
+
+            // Execute cmd
+            Execve(argv[cmds[i]], &argv[cmds[i]], environ);
+        }
+
+        // Parent (for each cmd)
+        if (pid != CHILD_PROCESS) {
+            childPids[i] = pid; // Save child pid
+
+            groupId = childPids[0]; // Set group id to first child pid
+            Setpgid(childPids[i], groupId);
+        }    
+    }
+
+    // Parent closes pipes
+    for (int i = 0; i < numCmds - 1; i++) {
+        Close(pipes[i][READ_END]);
+        Close(pipes[i][WRITE_END]);
+    }
+
+    // Parent waits for kids
+    for (int i = 0; i < numCmds; i++) {
+        int status;
+        Waitpid(childPids[i], &status, 0);
+    }
+    
     return;
 }
 
@@ -294,7 +404,15 @@ int parseline(const char *cmdline, char **argv)
  */
 int builtin_cmd(char **argv) 
 {
-    return 0;     /* not a builtin command */
+    if (strcmp(argv[0], "quit") == 0)
+        exit(EXIT_SUCCESS);
+    if (strcmp(argv[0], "fg") == 0)
+        return TRUE;
+    if (strcmp(argv[0], "bg") == 0)
+        return TRUE;
+    if (strcmp(argv[0], "jobs") == 0)
+        return TRUE;
+    return FALSE;     /* not a builtin command */
 }
 
 /* 
@@ -570,4 +688,89 @@ void sigquit_handler(int sig)
 }
 
 
+/*
+ * EXTRA WRAPPER FUNCTIONS FOR SYSTEM CALLS - Taken from book - http://csapp.cs.cmu.edu/3e/ics3/code/src/csapp.c
+ */
+pid_t Fork(void) 
+{
+    pid_t pid;
 
+    if ((pid = fork()) < 0)
+	unix_error("Fork error");
+    return pid;
+}
+
+int Dup2(int fd1, int fd2) 
+{
+    int rc;
+
+    if ((rc = dup2(fd1, fd2)) < 0)
+	unix_error("Dup2 error");
+    return rc;
+}
+
+int Pipe(int* pipedes) {
+    int rc;
+
+    if ((rc = pipe(pipedes) < 0)) {
+        unix_error("Pipe error");
+    }
+    return rc;
+}
+
+void Execve(const char *filename, char *const argv[], char *const envp[]) 
+{
+    if (execve(filename, argv, envp) < 0)
+	unix_error("Execve error");
+}
+
+pid_t Wait(int *status) 
+{
+    pid_t pid;
+
+    if ((pid  = wait(status)) < 0)
+	unix_error("Wait error");
+    return pid;
+}
+
+pid_t Waitpid(pid_t pid, int *iptr, int options) 
+{
+    pid_t retpid;
+
+    if ((retpid  = waitpid(pid, iptr, options)) < 0) 
+	unix_error("Waitpid error");
+    return(retpid);
+}
+
+FILE* Fopen(const char *filename, const char *mode) 
+{
+    FILE *fp;
+
+    if ((fp = fopen(filename, mode)) == NULL) {
+	    unix_error("Fopen error"); 
+    }
+
+    return fp;
+}
+
+void Fclose(FILE *fp) 
+{
+    if (fclose(fp) != 0)
+	unix_error("Fclose error");
+}
+
+void Setpgid(pid_t pid, pid_t pgid) {
+    int rc;
+
+    if ((rc = setpgid(pid, pgid)) < 0)
+	unix_error("Setpgid error");
+    return;
+}
+
+void Close(int fd) 
+{
+    int rc;
+
+    if ((rc = close(fd)) < 0)
+	unix_error("Close error");
+}
