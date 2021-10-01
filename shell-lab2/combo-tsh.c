a8e11dcb9940c31d01ba4588fff474f993b180be
Brigham Andersen <brighamband@gmail.com>
2021-10-01 11:34:34 -0600

    Got passing thru test 3 again with clean, only proj2 code

--- shell-lab2/combo-tsh.c
+++ shell-lab2/combo-tsh.c
@@ -94,8 +94,16 @@ handler_t *Signal(int signum, handler_t *handler);
 
 /* EXTRA WRAPPER FUNCTIONS FOR SYSTEM CALLS - See bottom of file for function bodies */
 pid_t Fork(void);
+int Dup2(int fd1, int fd2);
+int Pipe(int* pipedes);
+void Execve(const char *filename, char *const argv[], char *const envp[]);
+pid_t Wait(int *status);
 pid_t Waitpid(pid_t pid, int *iptr, int options);
+FILE* Fopen(const char *filename, const char *mode);
+void Fclose(FILE *fp);
 void Setpgid(pid_t pid, pid_t pgid);
+void Close(int fd);
+
 
 /*
  * main - The shell's main routine 
@@ -122,9 +130,9 @@ int main(int argc, char **argv)
             case 'p':             /* don't print a prompt */
                 emit_prompt = 0;  /* handy for automatic testing */
             break;
-        default:
-            usage();
-	    }
+            default:
+                usage();
+        }
     }
 
     /* Install the signal handlers */
@@ -143,7 +151,7 @@ int main(int argc, char **argv)
     /* Execute the shell's read/eval loop */
     while (1) {
 
-        /* Read command line */
+	    /* Read command line */
         if (emit_prompt) {
             printf("%s", prompt);
             fflush(stdout);
@@ -177,13 +185,18 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
-    char *argv[MAXARGS];    // argv[0] is the command
-    pid_t cpid = 0;  // Child process id
-    pid_t groupId = 0;    // Process group id
+    char *argv[MAXARGS];
+    pid_t pid;
+    int cmds[MAXARGS];
+    int stdin_redir[MAXARGS];
+    int stdout_redir[MAXARGS];
+    pid_t childPids[MAXARGS];
+    int pipes[MAXARGS][2];  // Making max number of pipes, can also just juggle 2 pipes like pipes[2].  Make sure to initialize numCmds - 1 pipes
+    pid_t groupId;
 
     int bg = parseline(cmdline, argv); 
 
-    // If no command is entered
+    // Ignore empty lines
     if (argv[0] == NULL)
         return;
 
@@ -191,33 +204,89 @@ void eval(char *cmdline)
     if (builtin_cmd(argv) == 1) 
         return;
 
-    cpid = Fork();
+    // If a custom (not built-in) cmd, run this
+    int numCmds = parseargs(argv, cmds, stdin_redir, stdout_redir); 
 
-    // Child
-    if (cpid == CHILD_PROCESS) {
-        // Execute cmd
-        if (execve(argv[0], argv, environ) < 0) {
-            printf("%s Command not found\n", argv[0]);
-            exit(EXIT_SUCCESS);
-        }
+    // Make pipes (num of pipes is 1 less than numCmds - they go between cmds)
+    for (int i = 0; i < numCmds - 1; i++) {
+        Pipe(pipes[i]);
     }
 
-    // Parent
-    if (cpid != CHILD_PROCESS) {
-        Setpgid(cpid, groupId);
+    for (int i = 0; i < numCmds; i++) {
+        pid = Fork();
 
-        // Fg
-        if (bg == FALSE) {
-            addjob(jobs, cpid, groupId, FG, cmdline);
-        } else {    // Bg
-            addjob(jobs, cpid, groupId, BG, cmdline);
+        // Child (for each cmd)
+        if (pid == CHILD_PROCESS) {
+            int redirectedInput = FALSE;
+            int redirectedOutput = FALSE;
+            
+            // If there's an input file, redirect stdin to input file
+            if (stdin_redir[i] > 0) {
+                redirectedInput = TRUE;
+                FILE *inFile = Fopen(argv[stdin_redir[i]], "r");
+                Dup2(fileno(inFile), STDIN_FILENO);
+                Fclose(inFile);
+            }
+
+            // If there's an output file, redirect stdout to output file
+            if (stdout_redir[i] > 0) {
+                redirectedOutput = TRUE;
+                FILE *outFile = Fopen(argv[stdout_redir[i]], "w");
+                Dup2(fileno(outFile), STDOUT_FILENO);
+                Fclose(outFile);
+            }
+
+            // Redirect stdout to write end (run this for all pipes but the last and if the output wasn't redirected to a file)
+            if (i < numCmds - 1 && !redirectedOutput) {
+                Dup2(pipes[i][WRITE_END], STDOUT_FILENO);
+            }
+
+            // Redirect stdin to read end (run this for all pipes but the first and if the input wasn't redirected to a file)
+            if (i > 0 && !redirectedInput) {
+                Dup2(pipes[i - 1][READ_END], STDIN_FILENO);
+            }
+        
+            // Close all pipes (even the ones you never used)
+            for (int j = 0; j < numCmds - 1; j++) {
+                Close(pipes[j][READ_END]);
+                Close(pipes[j][WRITE_END]);
+            }
+
+            // Execute cmd
+            Execve(argv[cmds[i]], &argv[cmds[i]], environ);
         }
 
-        // unblock processes
+        // Parent (for each cmd)
+        if (pid != CHILD_PROCESS) {
+            childPids[i] = pid; // Save child pid
 
-        // wait on pid
-    }    
+            groupId = childPids[0]; // Set group id to first child pid
+            Setpgid(childPids[i], groupId); // Set pgpid of each child to its parent's pid
+            // Adds job, depending if it's fg or bg
+            if (bg == True) {   // bg job
 
+            } else {    // fg job
+                addjob(jobs, childPids[i], groupId, ?, cmdline);
+            }
+        }    
+    }
+
+    // Parent closes pipes
+    for (int i = 0; i < numCmds - 1; i++) {
+        Close(pipes[i][READ_END]);
+        Close(pipes[i][WRITE_END]);
+    }
+
+    // Parent waits for kids
+    for (int i = 0; i < numCmds; i++) {
+        if (!bg) {
+            int status;
+            Waitpid(childPids[i], &status, 0);
+        } else {
+            printf("%d %s", pid, cmdline);
+        }
+    }
+    
     return;
 }
 
@@ -256,26 +325,26 @@ int parseargs(char **argv, int *cmds, int *stdin_redir, int *stdout_redir)
             argindex++;
             if (!argv[argindex]) { /* if we have reached the end, then break */
                 break;
-            }
+	    }
             stdin_redir[cmdindex] = argindex;
-        } else if (strcmp(argv[argindex], ">") == 0) {
+	} else if (strcmp(argv[argindex], ">") == 0) {
             argv[argindex] = NULL;
             argindex++;
             if (!argv[argindex]) { /* if we have reached the end, then break */
                 break;
-            }
+	    }
             stdout_redir[cmdindex] = argindex;
-        } else if (strcmp(argv[argindex], "|") == 0) {
+	} else if (strcmp(argv[argindex], "|") == 0) {
             argv[argindex] = NULL;
             argindex++;
             if (!argv[argindex]) { /* if we have reached the end, then break */
                 break;
-            }
+	    }
             cmdindex++;
             cmds[cmdindex] = argindex;
             stdin_redir[cmdindex] = -1;
             stdout_redir[cmdindex] = -1;
-	    }
+	}
         argindex++;
     }
 
@@ -307,8 +376,9 @@ int parseline(const char *cmdline, char **argv)
     if (*buf == '\'') {
         buf++;
         delim = strchr(buf, '\'');
-    } else {
-        delim = strchr(buf, ' ');
+    }
+    else {
+	    delim = strchr(buf, ' ');
     }
 
     while (delim) {
@@ -435,7 +505,7 @@ void initjobs(struct job_t *jobs) {
     int i;
 
     for (i = 0; i < MAXJOBS; i++)
-	    clearjob(&jobs[i]);
+	clearjob(&jobs[i]);
 }
 
 /* maxjid - Returns largest allocated job ID */
@@ -443,10 +513,9 @@ int maxjid(struct job_t *jobs)
 {
     int i, max=0;
 
-    for (i = 0; i < MAXJOBS; i++) {
-        if (jobs[i].jid > max)
-            max = jobs[i].jid;
-    }
+    for (i = 0; i < MAXJOBS; i++)
+	if (jobs[i].jid > max)
+	    max = jobs[i].jid;
     return max;
 }
 
@@ -499,10 +568,9 @@ int deletejob(struct job_t *jobs, pid_t pid)
 pid_t fgpid(struct job_t *jobs) {
     int i;
 
-    for (i = 0; i < MAXJOBS; i++) {
+    for (i = 0; i < MAXJOBS; i++)
         if (jobs[i].state == FG)
             return jobs[i].pid;
-    }
     return 0;
 }
 
@@ -512,10 +580,9 @@ struct job_t *getjobpid(struct job_t *jobs, pid_t pid) {
 
     if (pid < 1)
 	    return NULL;
-    for (i = 0; i < MAXJOBS; i++) {
+    for (i = 0; i < MAXJOBS; i++)
         if (jobs[i].pid == pid)
             return &jobs[i];
-    }
     return NULL;
 }
 
@@ -526,10 +593,9 @@ struct job_t *getjobjid(struct job_t *jobs, int jid)
 
     if (jid < 1)
 	    return NULL;
-    for (i = 0; i < MAXJOBS; i++) {
+    for (i = 0; i < MAXJOBS; i++)
         if (jobs[i].jid == jid)
             return &jobs[i];
-    }
     return NULL;
 }
 
@@ -542,7 +608,7 @@ int pid2jid(pid_t pid)
 	    return 0;
     for (i = 0; i < MAXJOBS; i++) {
         if (jobs[i].pid == pid) {
-            return jobs[i].jid;
+                return jobs[i].jid;
         }
     }
     return 0;
@@ -652,6 +718,39 @@ pid_t Fork(void)
     return pid;
 }
 
+int Dup2(int fd1, int fd2) 
+{
+    int rc;
+
+    if ((rc = dup2(fd1, fd2)) < 0)
+	    unix_error("Dup2 error");
+    return rc;
+}
+
+int Pipe(int* pipedes) {
+    int rc;
+
+    if ((rc = pipe(pipedes) < 0)) {
+        unix_error("Pipe error");
+    }
+    return rc;
+}
+
+void Execve(const char *filename, char *const argv[], char *const envp[]) 
+{
+    if (execve(filename, argv, envp) < 0)
+	    unix_error("Execve error");
+}
+
+pid_t Wait(int *status) 
+{
+    pid_t pid;
+
+    if ((pid  = wait(status)) < 0)
+	    unix_error("Wait error");
+    return pid;
+}
+
 pid_t Waitpid(pid_t pid, int *iptr, int options) 
 {
     pid_t retpid;
@@ -661,10 +760,35 @@ pid_t Waitpid(pid_t pid, int *iptr, int options)
     return(retpid);
 }
 
+FILE* Fopen(const char *filename, const char *mode) 
+{
+    FILE *fp;
+
+    if ((fp = fopen(filename, mode)) == NULL) {
+	    unix_error("Fopen error"); 
+    }
+
+    return fp;
+}
+
+void Fclose(FILE *fp) 
+{
+    if (fclose(fp) != 0)
+	    unix_error("Fclose error");
+}
+
 void Setpgid(pid_t pid, pid_t pgid) {
     int rc;
 
     if ((rc = setpgid(pid, pgid)) < 0)
 	    unix_error("Setpgid error");
     return;
+}
+
+void Close(int fd) 
+{
+    int rc;
+
+    if ((rc = close(fd)) < 0)
+	    unix_error("Close error");
 }
