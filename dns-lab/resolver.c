9e72f8b846ddf7821fba759c8b5c27a1ee2d1d84
Brigham Andersen <brighamband@gmail.com>
2021-10-25 13:23:32 -0600

    added client, hafta fix

--- dns-lab/resolver.c
+++ dns-lab/resolver.c
@@ -292,6 +292,82 @@ int send_recv_message(unsigned char *request, int requestlen, unsigned char *res
 	 *             response should be received
 	 * OUTPUT: the size (bytes) of the response received
 	 */
+	#define BUF_SIZE 500
+
+	struct addrinfo hints;
+	struct addrinfo *result, *rp;
+	int hostindex = 1;
+	int sfd, s, j;
+	size_t len;
+	ssize_t nread;
+	char buf[BUF_SIZE];
+	int af = AF_UNSPEC;
+
+	/* Obtain address(es) matching host/port */
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+	hints.ai_flags = 0;
+	hints.ai_protocol = 0;          /* Any protocol */
+
+	s = getaddrinfo(argv[hostindex], argv[hostindex + 1], &hints, &result);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
+
+	/* getaddrinfo() returns a list of address structures.
+	   Try each address until we successfully connect(2).
+	   If socket(2) (or connect(2)) fails, we (close the socket
+	   and) try the next address. */
+
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
+		sfd = socket(rp->ai_family, rp->ai_socktype,
+				rp->ai_protocol);
+		if (sfd == -1)
+			continue;
+
+		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
+			break;                  /* Success */
+
+		close(sfd);
+	}
+
+	if (rp == NULL) {               /* No address succeeded */
+		fprintf(stderr, "Could not connect\n");
+		exit(EXIT_FAILURE);
+	}
+
+	freeaddrinfo(result);           /* No longer needed */
+
+	/* Send remaining command-line arguments as separate
+	   datagrams, and read responses from server */
+
+	for (j = hostindex + 2; j < argc; j++) {
+		len = strlen(argv[j]) + 1;
+		/* +1 for terminating null byte */
+
+		if (len + 1 > BUF_SIZE) {
+			fprintf(stderr,
+					"Ignoring long message in argument %d\n", j);
+			continue;
+		}
+
+		if (write(sfd, argv[j], len) != len) {
+			fprintf(stderr, "partial/failed write\n");
+			exit(EXIT_FAILURE);
+		}
+		printf("Sent %ld bytes to server\n", len);
+
+		nread = read(sfd, buf, BUF_SIZE);
+		if (nread == -1) {
+			perror("read");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("Received %zd bytes: %s\n", nread, buf);
+	}
 }
 
 dns_answer_entry *resolve(char *qname, char *server, char *port) {
@@ -306,15 +382,17 @@ dns_answer_entry *resolve(char *qname, char *server, char *port) {
 	// Print byte wire (debugging purposes)
 	print_bytes(wire, wireLen);
 
-	// Send off query wire
-	
-	// send_recv_message(request, requestLen, response, server, port);
+	// Send off query wire from client to server using UDP
+	unsigned char* response = (unsigned char *) malloc(MAX_SIZE);
+	int responseLen = send_recv_message(request, requestLen, response, server, port);
 
 	// Extract answer from response
 	// get_answer_address(qname, qtype, response);
 	
 
+	// Clean up for malloc
 	free(wire);
+	// free(response);
 }
 
 int main(int argc, char *argv[]) {
