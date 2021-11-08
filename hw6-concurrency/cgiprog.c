7f2089a64c996ef099165382b99e11d2a7f9863a
Brigham Andersen <brighamband@gmail.com>
2021-11-08 14:11:38 -0700

    fixed cgiprog

--- hw6-concurrency/cgiprog.c
+++ hw6-concurrency/cgiprog.c
@@ -1,26 +1,15 @@
 #include "csapp.h"
 
 int main() {
-  char* buf;
-  char* p;
-  char content[MAXLINE];
-  char query_str[MAXLINE];
-
-  /* Extract the two arguments */
-  if ((buf = getenv("QUERY_STRING")) != NULL) {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(query_str, buf);
-  } 
-
-  // Make response body
-  sprintf(content, "The query string is: %s\r\n", query_str);
+  // Get query from env
+  char *query_str = getenv("QUERY_STRING");
 
   // Make HTTP response
-  printf("Content-length: %d\r\n", (int)strlen(content)); 
-  printf("Content-type: text/plain\r\n\r\n"); 
-  printf("%s", content);  // Add on response body from above
-  fflush(stdout);
+  // Include response headers
+  printf("Content-length: %d\r\n", (int)strlen(query_str)); 
+  printf("Content-type: text/plain\r\n\r\n");
+  // Include response body
+  printf("The query string is: %s\n", query_str);
 
   return 0;
 }
