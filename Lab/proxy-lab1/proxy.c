b618b84c6a40d9f7c1122d532dffbef36084b294
Brigham Andersen <brighamband@gmail.com>
2021-11-08 19:32:56 -0700

    proxy skeleton set up

--- Lab/proxy-lab1/proxy.c
+++ Lab/proxy-lab1/proxy.c
@@ -7,8 +7,21 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
-{
+// START - ECHO THREAD SERVER CODE - echoservert.c
+// Step 1
+void client_req_to_proxy() {}
+
+// Step 2
+void proxy_req_to_server() {}    
+
+// Step 3
+void server_res_to_proxy() {}    
+
+// Step 4
+void proxy_res_to_client() {}    
+
+int main() {
+    
     // 1 - receive request from client
     // 2 - send off request to server
 
